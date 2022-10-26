67176416024e9427e4a174e74b602e545839db3f
Thomas Henley <t4.crow@gmail.com>
2022-10-26 14:55:57 -0400

    initial commit

---
+++ Chapter02/Arrays/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+
+// See https://aka.ms/new-console-template for more information
+string[] names;
+names = new string[4];
+names[0] = "Kate";
+names[1] = "Jack";
+names[2] = "Rebecca";
+names[3] = "Tom";
+
+string[] names2 = { "Kate", "Jack", "Rebecca", "Tom" };
+
+for (int i = 0; i < names2.Length; i++)
+{
+    Console.WriteLine(names2[i]);
+}
+
+int max = 500;
+for (byte i = 0; i < max; i++)
+{
+    WriteLine(i);
+}
---
+++ Chapter04/WritingFunctions/Program.cs
@@ -0,0 +1,49 @@
+﻿using static System.Console;
+// See https://aka.ms/new-console-template for more information
+
+static string CardinalToOrdinal(int number)
+{
+  switch (number)
+  {
+    case 11: // special cases for 11th to 13th
+    case 12:
+    case 13:
+      return $"{number}th";
+    default:
+      int lastDigit = number % 10;
+      string suffix = lastDigit switch
+      {
+        1 => "st",
+        2 => "nd",
+        3 => "rd",
+        _ => "th"
+      };
+      return $"{number}{suffix}";
+  }
+}
+
+static int FibImperative(int term)
+{
+    if (term == 1)
+    {
+        return 0;
+    }
+    else if (term == 2)
+    {
+        return 1;
+    }
+    else
+    {
+        return FibImperative(term - 1) + FibImperative(term - 2);
+    }
+}
+
+static void RunFibImperative()
+{
+    for (int i = 1; i <= 30; i++)
+    {
+        WriteLine($"The {CardinalToOrdinal(i)} term of the Fibonacci sequence is {FibImperative(term: i)}.");
+    }
+}
+
+RunFibImperative();
