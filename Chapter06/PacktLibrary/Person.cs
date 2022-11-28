8bafc0b0faa96aaaf3e51ce345c70a561e61ab61
Thomas Henley <thenley@ptc.com>
2022-11-28 16:25:23 -0500

    begin chapter 6

---
+++ Chapter06/PacktLibrary/Person.cs
@@ -0,0 +1,62 @@
+﻿#nullable enable
+using static System.Console;
+
+namespace PacktLibrary;
+
+public class Person : object, IComparable<Person>
+{
+    // fields
+    public string? Name;
+    public DateTime DateOfBirth;
+    public List<Person> Children = new();
+
+    // methods
+    public void WriteToConsole()
+    {
+        WriteLine($"{Name} was born on a {DateOfBirth:dddd}.");
+    }
+
+    public static Person Procreate(Person p1, Person p2)
+    {
+        Person baby = new()
+        {
+            Name = $"Baby of {p1.Name} and {p2.Name}"
+        };
+        p1.Children.Add(baby);
+        p2.Children.Add(baby);
+        return baby;
+    }
+
+    public static Person operator *(Person p1, Person p2)
+    {
+        return Procreate(p1, p2);
+    }
+
+    public Person ProcreateWith(Person partner)
+    {
+        return Procreate(this, partner);
+    }
+
+    // delegate field
+    public event EventHandler? Shout;
+
+    // data field
+    public int AngerLevel;
+
+    // method
+    public void Poke()
+    {
+        AngerLevel++;
+        if (AngerLevel >= 3)
+        {
+            Shout?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    public int CompareTo(Person? other)
+    {
+        if (ReferenceEquals(this, other)) return 0;
+        if (ReferenceEquals(null, other)) return 1;
+        return string.Compare(Name, other.Name, StringComparison.Ordinal);
+    }
+}
---
+++ Chapter06/PeopleApp/Program.cs
@@ -0,0 +1,77 @@
+﻿using PacktLibrary;
+using static System.Console;
+
+Person harry = new() { Name = "Harry" };
+Person mary = new() { Name = "Mary" };
+Person jill = new() { Name = "Jill" };
+
+// call instance methods
+Person baby1 = mary.ProcreateWith(harry);
+baby1.Name = "Gary";
+
+// call static methods
+Person baby2 = Person.Procreate(harry, jill);
+Person baby3 = harry * mary;
+WriteLine($"{harry.Name} has {harry.Children.Count} children.");
+WriteLine($"{mary.Name} has {mary.Children.Count} children.");
+WriteLine($"{jill.Name} has {jill.Children.Count} children.");
+WriteLine(
+    format: "{0}'s first child is named \"{1}\".",
+    arg0: harry.Name,
+    arg1: harry.Children.First().Name);
+
+static void Harry_Shout(object? sender, EventArgs e)
+{
+    if (sender is null) return;
+    Person p = (Person)sender;
+    WriteLine($"{p.Name} is this angry: {p.AngerLevel}");
+}
+
+static void Harry_Shout_2(object? sender, EventArgs e)
+{
+    if (sender is null) return;
+    Person p = (Person)sender;
+    WriteLine($"{p.Name} says \"Alright, cut the shit buddy\"");
+}
+
+harry.Shout += Harry_Shout;
+harry.Shout += Harry_Shout_2;
+
+for (int i = 0; i < 4; i++)
+{
+    harry.Poke();
+}
+
+System.Collections.Hashtable lookupObject = new();
+lookupObject.Add(key: 1, value: "Alpha");
+lookupObject.Add(key: 2, value: "Beta");
+lookupObject.Add(key: 3, value: "Gamma");
+lookupObject.Add(key: harry, value: "Delta");
+
+int key = 2;
+WriteLine(format: "Key {0} has value {1}",
+    arg0: key,
+    arg1: lookupObject[key]);
+
+WriteLine(format: "Key {0} has value {1}",
+    arg0: harry,
+    arg1: lookupObject[harry]);
+
+Person[] people =
+{
+    new() { Name = "Simon" },
+    new() { Name = "Jenny" },
+    new() { Name = "Adam" },
+    new() { Name = "Richard" }
+};
+WriteLine("Initial list of people:");
+foreach (Person p in people)
+{
+    WriteLine($"  {p.Name}");
+}
+WriteLine("Use Person's IComparable implementation to sort:");
+Array.Sort(people);
+foreach (Person p in people)
+{
+    WriteLine($"  {p.Name}");
+}
