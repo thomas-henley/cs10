631f7703f492609ca99f5c9da77ef82b0efba75b
Thomas Henley <thenley@ptc.com>
2022-12-16 11:39:52 -0500

    finish chapter 09

---
+++ Chapter09/WorkingWIthStreams/Program.cs
@@ -0,0 +1,86 @@
+﻿using System.Xml;
+using static System.Console;
+using static System.Environment;
+using static System.IO.Path;
+
+WorkWithText();
+WorkWithXml();
+
+static void WorkWithText()
+{
+    string textFile = Combine(CurrentDirectory, "streams.txt");
+    StreamWriter text = File.CreateText(textFile);
+    foreach (string item in Viper.Callsigns)
+    {
+        text.WriteLine(item);
+    }
+    text.Close();
+    
+    WriteLine("{0} contains {1:N0} bytes.",
+        arg0: textFile,
+        arg1: new FileInfo(textFile).Length);
+    WriteLine(File.ReadAllText(textFile));
+}
+
+static void WorkWithXml()
+{
+    FileStream? xmlFileStream = null;
+    XmlWriter? xml = null;
+    try
+    {
+        // define a file to write to
+        string xmlFile = Combine(CurrentDirectory, "streams.xml");
+        // create a file stream
+        xmlFileStream = File.Create(xmlFile);
+        // wrap the file stream in an XML writer helper
+        // and automatically indent nested elements
+        xml = XmlWriter.Create(xmlFileStream,
+            new XmlWriterSettings { Indent = true });
+        // write the XML declaration
+        xml.WriteStartDocument();
+        // write a root element
+        xml.WriteStartElement("callsigns");
+        // enumerate the strings writing each one to the stream
+        foreach (string item in Viper.Callsigns)
+        {
+            xml.WriteElementString("callsign", item);
+        }
+        // write the close root element
+        xml.WriteEndElement();
+        // close helper and stream
+        xml.Close();
+        xmlFileStream.Close();
+        // output all the contents of the file
+        WriteLine("{0} contains {1:N0} bytes.",
+            arg0: xmlFile,
+            arg1: new FileInfo(xmlFile).Length);
+        WriteLine(File.ReadAllText(xmlFile));
+    }
+    catch (Exception ex)
+    {
+        // if the path doesn't exist the exception will be caught
+        WriteLine($"{ex.GetType()} says {ex.Message}");
+    }
+    finally
+    {
+        if (xml != null)
+        {	
+            xml.Dispose();
+            WriteLine("The XML writer's unmanaged resources have been disposed.");
+            if (xmlFileStream != null)
+            {
+                xmlFileStream.Dispose();
+                WriteLine("The file stream's unmanaged resources have been disposed.");
+            }
+        }
+    }
+}
+
+static class Viper
+{
+    public static string[] Callsigns = new[]
+    {
+        "Husker", "Starbuck", "Apollo", "Boomer",
+        "Bulldog", "Athena", "Helo", "Racetrack"
+    };
+}
---
+++ Chapter09/WorkingWithSerialization/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Xml.Serialization;
+using WorkingWithSerialization;
+using static System.Console;
+using static System.Environment;
+using static System.IO.Path;
+
+// create an object graph
+List<Person> people = new()
+{
+    new(30000M) 
+    {
+        FirstName = "Alice",
+        LastName = "Smith",
+        DateOfBirth = new(1974, 3, 14)
+    },
+    new(40000M) 
+    {
+        FirstName = "Bob",
+        LastName = "Jones",
+        DateOfBirth = new(1969, 11, 23)
+    },
+    new(20000M)
+    {
+        FirstName = "Charlie",
+        LastName = "Cox",
+        DateOfBirth = new(1984, 5, 4),
+        Children = new()
+        {
+            new(0M)
+            {
+                FirstName = "Sally",
+                LastName = "Cox",
+                DateOfBirth = new(2000, 7, 12)
+            }
+        }
+    }
+};
+
+XmlSerializer xs = new(people.GetType());
+string path = Combine(CurrentDirectory, "people.xml");
+using (FileStream stream = File.Create(path))
+{
+    xs.Serialize(stream, people);
+}
+WriteLine("Written {0:N0} bytes of XML to {1}",
+    arg0: new FileInfo(path).Length,
+    arg1: path);
+WriteLine();
+WriteLine(File.ReadAllText(path));
